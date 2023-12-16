eae7ccee27454bd02e98c71ed2122d51dbac25b6
MelvinSam2000 <melvinsamxd@gmail.com>
2023-12-16 11:32:28 -0500

    Fixed RNN by making all weights the same accross time

--- src/rnncell.rs
+++ src/rnncell.rs
@@ -7,18 +7,17 @@ use crate::activation::ActivationFunction;
 pub struct RnnCell<
     const X: usize,
     const Y: usize,
-    const HI: usize,
-    const HF: usize,
+    const H: usize,
+    const T: usize,
     F,
 > {
-    x: SVector<f64, X>,
-    wx: SMatrix<f64, HF, X>,
-    hi: SVector<f64, HI>,
-    wh: SMatrix<f64, HF, HI>,
-    z: SVector<f64, HF>,
-    hf: SVector<f64, HF>,
-    y: SVector<f64, Y>,
-    wy: SMatrix<f64, Y, HF>,
+    x: [SVector<f64, X>; T],
+    y: [SVector<f64, Y>; T],
+    h: [SVector<f64, H>; T],
+    z: [SVector<f64, H>; T],
+    wx: SMatrix<f64, H, X>,
+    wh: SMatrix<f64, H, H>,
+    wy: SMatrix<f64, Y, H>,
     learn_rate: f64,
     act: F,
 }
@@ -26,22 +25,21 @@ pub struct RnnCell<
 impl<
         const X: usize,
         const Y: usize,
-        const HI: usize,
-        const HF: usize,
+        const H: usize,
+        const T: usize,
         F,
-    > RnnCell<X, Y, HI, HF, F>
+    > RnnCell<X, Y, H, T, F>
 where
-    F: ActivationFunction<HF>,
+    F: ActivationFunction<H>,
 {
     pub fn new(
         learn_rate: f64,
         activation_function: F,
     ) -> Self {
-        let x = SVector::zeros();
-        let y = SVector::zeros();
-        let hi = SVector::zeros();
-        let hf = SVector::zeros();
-        let z = SVector::zeros();
+        let x = [SVector::zeros(); T];
+        let y = [SVector::zeros(); T];
+        let h = [SVector::zeros(); T];
+        let z = [SVector::zeros(); T];
 
         let mut wx = SMatrix::zeros();
         let mut wy = SMatrix::zeros();
@@ -50,18 +48,18 @@ where
         // randomize weights
         let mut rng = rand::thread_rng();
         let uniform = rand_distr::Uniform::new(-1.0, 1.0);
-        for i in 0..HF {
+        for i in 0..H {
             for j in 0..X {
                 wx[(i, j)] = rng.sample(uniform);
             }
         }
         for i in 0..Y {
-            for j in 0..HF {
+            for j in 0..H {
                 wy[(i, j)] = rng.sample(uniform);
             }
         }
-        for i in 0..HF {
-            for j in 0..HI {
+        for i in 0..H {
+            for j in 0..H {
                 wh[(i, j)] = rng.sample(uniform);
             }
         }
@@ -70,52 +68,61 @@ where
 
         Self {
             x,
-            wx,
-            hi,
-            wh,
-            z,
-            hf,
             y,
+            h,
+            wx,
+            wh,
             wy,
+            z,
             learn_rate,
             act,
         }
     }
 
     // feedforward
-    pub fn ff(
-        &mut self,
-        x: SVector<f64, X>,
-        hi: SVector<f64, HI>,
-    ) -> (SVector<f64, Y>, SVector<f64, HF>) {
+    pub fn ff(&mut self, x: [SVector<f64, X>; T]) {
         self.x = x;
-        self.hi = hi;
-        self.z = self.wx * self.x + self.wh * self.hi;
-        self.hf = self.act.func(&self.z);
-        self.y = self.wy * self.hf;
-        (self.y, self.hf)
+        for t in 0..T {
+            self.z[t] = self.wx * self.x[t]
+                + if t != 0 {
+                    self.wh[t] * self.h[t - 1]
+                } else {
+                    SVector::zeros()
+                };
+            self.h[t] = self.act.func(&self.z[t]);
+            self.y[t] = self.wy * self.h[t];
+        }
     }
 
     // backprop
-    pub fn bp(
-        &mut self,
-        gy: SVector<f64, Y>,
-        gh: SVector<f64, HF>,
-    ) -> SVector<f64, HI> {
-        let wy_copy = self.wy.clone();
-        let wh_copy = self.wh.clone();
-        self.wy -=
-            self.learn_rate * gy * self.hf.transpose();
-        let gy = wy_copy.transpose() * gy;
-        let g = gy + gh;
-        let dhfdz = SMatrix::from_diagonal(
-            &self.act.deriv(&self.z),
-        );
-        let g = dhfdz * g;
-        let gh = wh_copy.transpose() * &g;
-        self.wx -= self.learn_rate * g * self.x.transpose();
-        self.wh -=
-            self.learn_rate * g * self.hi.transpose();
-        gh
+    pub fn bp(&mut self, gl: [SVector<f64, Y>; T]) {
+        let wy_old = self.wy.clone();
+        let wh_old = self.wh.clone();
+
+        // update Wy
+        for t in 0..T {
+            self.wy -= self.learn_rate
+                * gl[t]
+                * self.h[t].transpose();
+        }
+
+        // update Wx and Wh
+        let mut g = SVector::zeros();
+        for t in (0..T).rev() {
+            let dht_dzt = SMatrix::from_diagonal(
+                &self.act.deriv(&self.z[t]),
+            );
+            let g_tmp =
+                dht_dzt * (wy_old.transpose() * gl[t] + &g);
+            self.wx -= self.learn_rate
+                * g_tmp
+                * self.x[t].transpose();
+            if t != 0 {
+                self.wh -= self.learn_rate
+                    * g_tmp
+                    * self.h[t - 1].transpose();
+            }
+            g += wh_old.transpose() * g_tmp;
+        }
     }
 }
